23a7a4d90f3b92e6fea6dbe8624cc8dc82496a21
Delymar Rodriguez <rodriguezdelymar@gmail.com>
2016-11-13 09:53:09 -0400

    add cubeService

--- app/landing/landing.controller.js
+++ app/landing/landing.controller.js
@@ -20,40 +20,79 @@
          vm.mostrarUpdate = [];
          vm.resultados =new Array();
 
-         vm.next= function() {
-            vm.T = vm.form.cs;
-            vm.M = vm.form.m;
-            N = vm.form.n;
-            vm.generateCube(N);
-            vm.mostrar = true;
-            vm.result = false;
-          //  vm.update(1, 1, 1, 3);
-          //  vm.summation(0, 1, 1, 1, 1, 1);
+          vm.available = function() {
+            if(_.size(vm.form) == 3){
+              if(/^([0-9])*$/.test(vm.form.cs) && /^([0-9])*$/.test(vm.form.m) &&  /^([0-9])*$/.test(vm.form.n)){
+                if(vm.form.cs != 0 && vm.form.m != 0 && vm.form.n != 0){
+                  return false;
+                } else{
+                  return true;
+                }
+              } else{
+                return true;
+              }
+            } else{
+             return true;
+            }
+          }
+
+         vm.next = function() {
+             vm.T = vm.form.cs;
+             vm.M = vm.form.m;
+             N = vm.form.n;
+             vm.generateCube(N);
+             vm.mostrar = true;
+             vm.result = false;
+             vm.aux = 0;
+             vm.proced = new Array();
+             vm.resultados = new Array();
          }
+
+        vm.validatePos = function(x, y, z){
+          if (x > N || y > N || z > N || x < 1 || y < 1 || z < 1 )
+            return false;
+          else{
+              if(/^([0-9])*$/.test(x) && /^([0-9])*$/.test(y) &&  /^([0-9])*$/.test(z)){
+                return true;
+              }
+              else{
+                swal("Alerta", "La posición ["+x+","+y+","+z+"] no existe", "warning");
+              }
+          }
+
+        }
 
          vm.calculate = function(){
            if(!_.isEmpty(vm.proced)){
              for (j = 0 ; j < vm.T ; j++) {
                  var proc = _.map(vm.proced[j], _.clone);
                  if(!_.isEmpty(proc[0]) || proc[0] != undefined){
-                   _.forEach(proc[0], function(operacion){
-                     if(operacion.type == 'U'){
-                       return vm.update(operacion.x, operacion.y, operacion.z, operacion.w);
-                     }
-                     if(operacion.type == 'Q'){
-                       vm.resultados.push(vm.summation(operacion.x1, operacion.y1, operacion.z1, operacion.x2, operacion.y2, operacion.z2));
-                    }
-                  });
-                } else {
-                    vm.result=false;
-                    return swal("Alerta", "Falta una operación por completar", "warning");
+                   if( _.size(proc[0]) === vm.M){
+                     _.forEach(proc[0], function(operacion){
+                       if(operacion.type == 'U'){
+                         return vm.update(operacion.x, operacion.y, operacion.z, operacion.w);
+                       }
+                       if(operacion.type == 'Q'){
+                         vm.resultados.push(vm.summation(operacion.x1, operacion.y1, operacion.z1, operacion.x2, operacion.y2, operacion.z2));
+                      }
+                    });
+                   }
+                   else{
+                     vm.result=false;
+                     return swal("Alerta", "Falta una operación por completar", "warning");
+                   }
                 }
                 vm.result=true;
             }
-
           }
            else {
             return swal("Alerta", "Debe seleccionar las operaciones", "warning");
+          }
+          if(!_.isEmpty(vm.resultados)){
+            return swal("Sumatoria de Cubo", vm.resultados)
+          }
+          else{
+            return swal("Sumatoria de Cubo", "No se realizo ningún Query")
           }
          }
 
@@ -90,10 +129,10 @@
          }
 
          vm.update = function (xx, yy, zz, w) {
-            if (xx > N || yy > N || zz > N || xx < 1 || yy < 1 || zz < 1 )
-              return swal("Posición ["+xx+","+yy+","+zz+"] no existe");
-            else
-              vm.cube[xx][yy][zz] = parseInt(w);
+              if (vm.validatePos(xx, yy, zz))
+                return vm.cube[xx][yy][zz] = parseInt(w);
+              else
+                swal("Alerta", "La posición ["+xx+","+yy+","+zz+"] no existe", "warning");
          }
 
          vm.print = function () {
@@ -107,11 +146,17 @@
          }
 
          vm.summation = function (x1, y1, z1, x2, y2, z2) {
-           if (x1 == x2 && y1 == y2 && z1 == z2) {
-             return vm.cube[x1][y1][z1]
+           if(!vm.validatePos(x1, y1, z1)){
+            return swal("Alerta", "La posición ["+x1+","+y1+","+z1+"] no existe", "warning");
+           }
+           if(!vm.validatePos(x2, y2, z2)){
+            return swal("Alerta", "La posición ["+x2+","+y2+","+z2+"] no existe", "warning");
            }
            if(x1 > x2){
              return swal("Alerta", "La posición inicial debe ser menor a la final", "warning");
+           }
+           if (x1 == x2 && y1 == y2 && z1 == z2) {
+             return vm.cube[x1][y1][z1]
            }
            vm.aux = 0;
            for (x = x1; x <= x2; x++){
---
+++ app/services/cubeService.js
@@ -0,0 +1,94 @@
+/**
+ * Created by Delymar on 07/09/2016.
+ */
+'use strict';
+(function(){
+    angular
+        .module('app')
+        .factory('cubeService', cubeService);
+
+    cubeService.$inject = [];
+
+    function cubeService() {
+
+        var cubeService = {
+            generate:generate,
+            update: update,
+            query: query,
+            print: print
+        };
+
+        return cubeService;
+
+        function generate(N) {
+          var deferred = $q.defer();
+          var x, y, z;
+          cube = new Array(N);
+          for (x = 1; x <= N; x++){
+              cube[x] = new Array(N);
+              for (y = 1; y <= N; y++) {
+                  cube[x][y] = new Array(N);
+                  for (z = 1; z <= N; z++){
+                    cube[x][y][z] = 0;
+                  }
+              }
+          }
+          deferred.resolve(cube);
+          return deferred.promise;
+        }
+
+        function update(cube, x, y, z, W) {
+          var deferred = $q.defer();
+          if (validatePos(x, y, z)) {
+            cube[x][y][z] = parseInt(W)
+            deferred.resolve(cube);
+          }
+          else {
+            deferred.reject({type:'warning', title: 'Alerta', description: 'La posición ['+x+','+y+','+z+'] no existe'});
+          }
+          return deferred.promise;
+        }
+
+        function validatePos(x, y, z) {
+          if (x > N || y > N || z > N || x < 1 || y < 1 || z < 1 ) {
+            return false;
+          }
+          else {
+            return (/^([0-9])*$/.test(x) && /^([0-9])*$/.test(y) &&  /^([0-9])*$/.test(z))
+            // if(/^([0-9])*$/.test(x) && /^([0-9])*$/.test(y) &&  /^([0-9])*$/.test(z)) {
+            //   return true;
+            // }
+            // else{
+            //   return false;
+            // }
+          }
+        }
+
+        function query (cube, x1, y1, z1, x2, y2, z2) {
+          var deferred = $q.defer();
+
+          if(!validatePos(x1, y1, z1))
+            deferred.reject({type:'warning', title: 'Alerta', description: 'La posición ['+x1+','+y1+','+z1+'] no existe'});
+
+          if(!validatePos(x2, y2, z2))
+            deferred.reject({type:'warning', title: 'Alerta', description: 'La posición ['+x2+','+y2+','+z2+'] no existe'});
+
+          if(x1 > x2)
+            deferred.reject({type:'warning', title: 'Alerta', description: 'La posición inicial debe ser menor a la final'});
+
+          if (x1 == x2 && y1 == y2 && z1 == z2)
+            deferred.resolve(cube[x1][y1][z1]);
+
+          var aux = 0;
+          for (x = x1; x <= x2; x++){
+              for (y = y1; y <= y2; y++){
+                  for (z = z1; z <= z2; z++){
+                    aux = cube[x][y][z] + aux;
+                  }
+              }
+          }
+          deferred.resolve(aux);
+          return deferred.promise;
+        }
+    }
+})();
